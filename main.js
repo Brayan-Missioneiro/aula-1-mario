0527d4a642f54fd9428929c8cf85446beadba66e
Brayan-Missioneiro <brayan.missioneiro@escola.pr.gov.br>
2026-08-14 12:29:06 +0000

    Atualização do projeto Mario

---
+++ main.js
@@ -0,0 +1,13 @@
+//Seleção de Elemento do html
+const audioStart = new Audio('.som/audio_theme.mp3')
+const audioGameOver = new Audio('/som/audio_gameover.mp3')
+const mario =document.queryselector('.mario');
+const pipe =document.queryselector('.pipe');
+const startButton =document.queryselector('.startButton');
+const restartButton =document.queryselector('.restartButton');
+const gameOver =document.queryselector('.gameOver');
+//Variaveis de Controle do jogo
+let = gameInterval = null;
+//Funcoes para Iniciar o jogo
+const startGame = () => {}
+pipe.classlits.add('pipe-animation')
